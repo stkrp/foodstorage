87fc0411c4ab071a663498fa38570b053ce9b1be
stkrp <stkrp@yandex.ru>
2016-02-08 14:18:12 +0300

    Добавлены ссылки на список оценок фотографии и список оценивших фотографию пользователей.

--- photos/serializers.py
+++ photos/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 from lib.serializers import CurrentUserDefaultSerializer
 
 from . import models
@@ -8,6 +9,8 @@ class _PhotoSerializer(
     CurrentUserDefaultSerializer, serializers.HyperlinkedModelSerializer
 ):
     avg_rating = serializers.SerializerMethodField()
+    ratings = serializers.SerializerMethodField()
+    rating_users = serializers.SerializerMethodField()
 
     class Meta:
         model = models.Photo
@@ -15,6 +18,20 @@ class _PhotoSerializer(
 
     def get_avg_rating(self, obj):
         return obj.avg_rating
+
+    def get_ratings(self, obj):
+        return reverse(
+            'photo-rating-list',
+            kwargs={'photo': obj.pk},
+            request=self.context.get('request')
+        )
+
+    def get_rating_users(self, obj):
+        return reverse(
+            'photo-rating-user-list',
+            kwargs={'photo': obj.pk},
+            request=self.context.get('request')
+        )
 
 
 class StaffPhotoSerializer(_PhotoSerializer):
